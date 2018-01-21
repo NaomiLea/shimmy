970d80d025121ac0469f74f26d3ac48b91a10713
Naomi K <naomik@hometalk.com>
2018-01-21 14:05:10 +0200

    adding node

--- script.js
+++ script.js
@@ -1,51 +1,14 @@
 var express = require('express');
+var path = require('path');
 var app = express();
-var server = require('http').createServer(app);
-var storage = [];
 
-// Sets the static file folders
-app.use("/js", express.static(__dirname + "/js"));
-app.use("/css", express.static(__dirname + "/css"));
+// Define the port to run on
+app.set('port', 3000);
 
-// Send & response function
+app.use(express.static(path.join(__dirname, 'public')));
 
-// set port
-var port = (process.env.PORT || 9000);
-server.listen(port, function() {
-	console.log('Server is running...');
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
 });
-
-
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-	console.log("Page requested");
-});
-
-
-
-// All other paths result in 404
-app.get('*', function(req, res) {
-	res.status(404).end();
-	// .sendFile(__dirname + '/404.html');
-});
-
-
-var nodemailer = require('nodemailer');
-
-var router = express.Router();
-app.use('/sayHello', router);
-router.post('/', handleSayHello); // handle the route at yourdomain.com/sayHello
-
-function handleSayHello(req, res) {
-    // Not the movie transporter!
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'naomikudren@gmail.com', // Your email id
-            pass: '#######' // Your password
-        }
-    });
-    ...
-    ...
-    ...
-}
