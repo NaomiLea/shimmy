8642bc9356cd08f7bd3d45604392be4db077af64
Naomi K <naomik@hometalk.com>
2018-01-21 14:41:24 +0200

    beginning of nodemailer

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var bodyParser = require('body-parser');
 
-// Define the port to run on
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.set('port', 3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Listen for requests
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
   console.log('Magic happens on port ' + port);
