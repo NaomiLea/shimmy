30a2813b7d5da74d0a06deaa0020f42aad285f85
Naomi K <naomik@hometalk.com>
2018-01-22 17:53:24 +0200

    hello

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var nodemailer = require("nodemailer");
 var smtpTransport = require('nodemailer-smtp-transport');
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-app.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+
+app.listen(process.env.PORT || 3000,function() {
+    console.log("App is running" );
 });
 
 
